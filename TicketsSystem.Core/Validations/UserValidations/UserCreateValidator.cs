d7610805aa07ef162d010cb0008466d51b600577
ElAguilaPrograma <je.munozm@outlook.com>
2026-01-24 04:03:26 -0600

    Client refactoring to eliminate the use of PrimeNG

---
+++ TicketsSystem.Core/Validations/UserValidations/UserCreateValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketsSystem.Core.Validations.UserValidations
+{
+    internal class UserCreateValidator
+    {
+    }
+}
